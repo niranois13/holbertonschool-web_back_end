2efc5238e01f1f1202c9674f0adfb9b234cd73dc
niranois13 <8784@holbertonstudents.com>
2024-08-19 10:31:14 +0200

    00-01

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module compiled wth Python3"""
+
+
+def add(num_a: float, num_b: float) -> float:
+    """
+    Function that adds two parameters given.
+    :param num_a: float - first number to add
+    :param num_b: float - second number to add
+    Returns: float - the sum
+    """
+    return num_a + num_b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module compiled with Python3"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function used to concatane two strings.
+    :param str1: str - first string to concatenate
+    :param str2: str - second string to concatenate
+    Returns: str - The concatenated result
+    """
+    return str1 + str2
