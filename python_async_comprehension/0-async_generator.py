fc64d5a0e1bafe60dd640cb511548d4f37060939
niranois13 <8784@holbertonstudents.com>
2024-08-21 13:53:33 +0200

    00 fixed

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An async coroutine generator that will run 10times,
     each time waiting for 1 second and then yeild a float number.
