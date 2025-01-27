22f7926a3b889c919ec655e3f3189341c0232f89
niranois13 <8784@holbertonstudents.com>
2025-01-27 15:20:52 +0100

    Task 4

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''MRUCache Module'''
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    inherits from BaseCaching and is a Most Recently Used caching system
+    """
+
+    def __init__(self):
+        """ Initiliazes an used_order variable
+        to keep track of the cache's key usage.
+        """
+        super().__init__()
+        self.used_order = []
+
+    def put(self, key, item):
+        """Add an item in the cache,
+        if the number of items is greater than the cache limit ('MAX_ITEMS=4'),
+        the most recently used item in cache gets discarded
+
+        :param key: The key for the cache entry.
+        :param item: The value associated with the key.
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.used_order.remove(key)
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            used_key = self.used_order.pop()
+            del self.cache_data[used_key]
+            print(f'DISCARD: {used_key}')
+
+        self.cache_data[key] = item
+        self.used_order.append(key)
+
+    def get(self, key):
+        """ Get an item by key,
+        the key is append to the used_order list
+        to keep track of the cache key usage
+
+        :param key: the key to be retrieved in the cache
+        :returns: The value associated to the key, None otherwise
+        """
+        if key not in self.cache_data or key is None:
+            return None
+
+        self.used_order.remove(key)
+        self.used_order.append(key)
+
+        return self.cache_data.get(key)
