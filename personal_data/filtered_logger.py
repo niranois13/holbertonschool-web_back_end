4a39aab785e4df35ba2cf70920e67de732936df7
niranois13 <8784@holbertonstudents.com>
2025-02-14 12:00:21 +0100

    Task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,18 +5,15 @@ Module filter_datum
 
 import re
 
-def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: list[str], redaction: str, message: str, separator: str
+        ) -> str:
     """
     filter_datum - module that returns a log message offuscated
-    :param fields: list[str] - representes all fields to obfuscate
-    :param redaction: str - represents by what the field will be obfuscated
-    :param message: str - the log line
-    :param separator: str - elements that separates each fields
     """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
-                        f'{field}={redaction}{separator}',
-                        message)
+                         f'{field}={redaction}{separator}',
+                         message)
     return message
-
-
