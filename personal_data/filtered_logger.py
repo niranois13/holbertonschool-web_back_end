6eb60b91d15d5ca9e122aa51e763335221abb954
niranois13 <8784@holbertonstudents.com>
2025-02-14 12:20:45 +0100

    Task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -33,8 +33,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields: List[str]):
+        """Initialize formatter with fields to redact."""
+        super().__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Filters values in incoming logs"""
