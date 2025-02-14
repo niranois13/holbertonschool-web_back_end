47811225d4957df1bc819b6bb15fa79c6e6ccc24
niranois13 <8784@holbertonstudents.com>
2025-02-14 12:03:01 +0100

    Task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,6 +11,10 @@ def filter_datum(
         ) -> str:
     """
     filter_datum - module that returns a log message offuscated
+    :param fields: list[str] - representes all fields to obfuscate
+    :param redaction: str - represents by what the field will be obfuscated
+    :param message: str - the log line
+    :param separator: str - elements that separates each fields
     """
     for field in fields:
         pattern = rf'{re.escape(field)}=[^ {re.escape(separator)}]*'
