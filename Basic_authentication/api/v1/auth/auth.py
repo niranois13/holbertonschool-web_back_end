a9857531358d1a0379bc18acfd924c5a60590bfb
niranois13 <8784@holbertonstudents.com>
2025-02-17 15:29:36 +0100

    Task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -41,9 +41,15 @@ class Auth:
         """
         authorization_header - returns None
         :param request: the Flask request object
-        Returns: None
+        Returns: the Authorization header or None
         """
-        return None
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> str:
         """
