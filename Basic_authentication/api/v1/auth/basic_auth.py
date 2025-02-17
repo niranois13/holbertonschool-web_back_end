a8fe50cfe6f80069d6255177270d17610344af7a
niranois13 <8784@holbertonstudents.com>
2025-02-17 15:44:24 +0100

    Task 6

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module of BasicAuth class
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth - Class that inherits from base class Auth
+
+    Does nothing
+    """
+    pass
