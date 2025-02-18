d11876e80bc50deaf70a24962559fecbb3b637c4
niranois13 <8784@holbertonstudents.com>
2025-02-18 11:41:49 +0100

    Task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,8 +13,7 @@ class BasicAuth(Auth):
     Sets up a Basic Authentication system
     """
     def extract_base64_authorization_header(
-            self, authorization_header: str
-            ) -> str:
+            self, authorization_header: str) -> str:
         """
         Function that return the Base64 part of the Authorization header
         :param authorization_header: str -
@@ -32,3 +31,23 @@ class BasicAuth(Auth):
                 'Basic ') else None
 
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Function that decodes a base64 authorization header
+        :param base_64_authorization_header: str - base64 string to be decoded
+        Returns:
+          - The decoded authorization header if properly formatted
+          - None otherwise
+        """
+        if base64_authorization_header is None or not isinstance(
+                                    base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_authorization_header = base64.b64decode(
+                                    base64_authorization_header, validate=True)
+            return decoded_authorization_header.decode("UTF-8")
+        except Exception:
+            return None
