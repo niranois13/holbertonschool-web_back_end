4dfb8bb50704b464900929b82e57ab9fd2a52041
niranois13 <8784@holbertonstudents.com>
2025-02-18 13:42:13 +0100

    Task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -75,7 +75,7 @@ class BasicAuth(Auth):
             return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
-                    self, user_email: str, user_pwd: str) -> TypeVar:
+                    self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """
         Function that returns a User instance based on its credentials
         :param user_email: str - email of an user
