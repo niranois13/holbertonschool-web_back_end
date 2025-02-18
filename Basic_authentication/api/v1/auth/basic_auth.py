59424342582161e2a53912848083cf337de5fab2
niranois13 <8784@holbertonstudents.com>
2025-02-18 11:12:29 +0100

    Task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,12 +3,32 @@
 Module of BasicAuth class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """
     BasicAuth - Class that inherits from base class Auth
 
-    Does nothing
+    Sets up a Basic Authentication system
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """
+        Function that return the Base64 part of the Authorization header
+        :param authorization_header: str -
+                            the authorization header to be manipulated
+        Returns:
+          - The base64 part of the Authorization header if properly formatted
+          - None otherwise
+        """
+        if authorization_header is None or not isinstance(
+                                    authorization_header, str):
+            return None
+
+        base64_part = authorization_header.removeprefix(
+            'Basic ') if authorization_header.startswith(
+                'Basic ') else None
+
+        return base64_part
