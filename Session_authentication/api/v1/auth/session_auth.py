a901562991b8a6511219f1570eee43c633b3ebe7
niranois13 <8784@holbertonstudents.com>
2025-02-20 12:00:59 +0100

    Task1

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module of SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth - Class that inherits from base class Auth
+
+    Sets up a Basic Session Authentication system
+    """
+    pass
