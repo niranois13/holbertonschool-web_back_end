5c4a159b1b60c198977367582e46b4673e41b927
niranois13 <8784@holbertonstudents.com>
2025-02-20 14:00:53 +0100

    Task5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -66,7 +66,11 @@ def auth_handler():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
