e3b1ab65ca738f1da0d8a5f225d452b88abf03cb
niranois13 <8784@holbertonstudents.com>
2025-02-20 13:55:41 +0100

    Task5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -45,6 +45,7 @@ def auth_handler():
     - '/api/v1/status/'
     - '/api/v1/unauthorized/'
     - '/api/v1/forbidden/'
+    - '/api/v1/auth_session/login/'
 
     Raises:
         401 Unauthorized: If the authorization header is missing or invalid.
@@ -59,12 +60,13 @@ def auth_handler():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
         ]
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
