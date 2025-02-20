8741e154c22c92f481dfcf32c0d0cb0f57271319
niranois13 <8784@holbertonstudents.com>
2025-02-20 15:54:42 +0100

    Task8

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -70,9 +70,21 @@ class SessionAuth(Auth):
           - True, on deletion
           - False, otherwise
           """
-        try:
-            session_id = self.session_cookie(request)
-            del self.user_id_by_session_id[session_id]
-            return True
-        except Exception:
+        if request is None:
             return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
+
+        return True
