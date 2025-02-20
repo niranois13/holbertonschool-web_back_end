218a3e85216af569f6669a489b7a45636314dc4e
niranois13 <8784@holbertonstudents.com>
2025-02-20 15:33:50 +0100

    Task8

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -61,3 +61,25 @@ class SessionAuth(Auth):
             return User.get(user_id)
         except Exception:
             return None
+
+    def destroy_session(self, request=None):
+        """
+        Function that destroys a user session, ensuring logout
+        :param request: the Flask request object
+        Returns:
+          - True, on deletion
+          - False, otherwise
+          """
+        if not request:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        if not self.user_id_for_session_id(session_id):
+            return False
+
+        del self.user_id_by_session_id(session_id)
+
+        return True
