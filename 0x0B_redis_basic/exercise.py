5d60bf2e0e63f4f8b9a77a6e8956073949c8084e
niranois13 <8784@holbertonstudents.com>
2025-03-20 12:32:45 +0100

    Task 3-5

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,6 +17,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a function."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+
+        return result
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """Initialize a Cache instance and flush the database"""
@@ -24,6 +39,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the given data in Redis with a uuid key and returns the key"""
         key = str(uuid.uuid4())
@@ -52,3 +68,18 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         """Retrieve an integer from Redis."""
         return self.get(key, int)
+
+    def replay(self, method: Callable):
+        """Display the history of calls of a particular function."""
+        meth_name = method.__qualname__
+        input_key = f"{meth_name}:inputs"
+        output_key = f"{meth_name}:outputs"
+
+        inputs = self._redis.lrange(input_key, 0, -1)
+        outputs = self._redis.lrange(output_key, 0, -1)
+
+        print(f"{meth_name} was called {len(inputs)} times:")
+        for inp, out in zip(inputs, outputs):
+            print(
+                f"{meth_name}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}"
+                )
