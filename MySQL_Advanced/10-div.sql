4bf5d4c28a8d1ea365e04d04ba7bbc362285d834
niranois13 <8784@holbertonstudents.com>
2025-03-21 15:19:58 +0100

    Task7-11

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- 10. Safe divide
+-- 10. Safe divide
+DELIMITER |
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    DECLARE result FLOAT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END;
+|
---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,3 @@
+-- 8. Optimize simple search
+-- 8. Optimize simple search
+CREATE INDEX idx_name_first ON names ( name(1) );
