12da4dc62f4411dd21e6a179a1629c03b2b99e9c
niranois13 <8784@holbertonstudents.com>
2025-03-21 11:07:50 +0100

    Task0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,7 @@
-CREATE TABLE users (
+-- 0. We are all unique!
+-- 0. We are all unique!
+
+CREATE TABLE IF NOT EXISTS 'users' (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     email VARCHAR(255) UNIQUE
