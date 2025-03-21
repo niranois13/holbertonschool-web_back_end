0ee8d12ab105f480efa39aa121e7ec6f3baa7cac
niranois13 <8784@holbertonstudents.com>
2025-03-21 11:09:53 +0100

    Task0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- 0. We are all unique!
 -- 0. We are all unique!
 
-CREATE TABLE IF NOT EXISTS 'users' (
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'name' VARCHAR(255),
-    'email' VARCHAR(255) NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `email` VARCHAR(255) NOT NULL UNIQUE
 );
