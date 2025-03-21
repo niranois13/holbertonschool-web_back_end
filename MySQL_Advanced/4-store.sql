2d7e985c026f40c057d3383af24e696460686710
niranois13 <8784@holbertonstudents.com>
2025-03-21 13:21:15 +0100

    Task4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,12 +1,7 @@
 -- 4. Buy buy buy
 -- 4. Buy buy buy
-
-DELIMITER |
 CREATE TRIGGER stock_handle BEFORE INSERT ON orders
-    FOR EACH ROW 
-    BEGIN
-        UPDATE items
-        SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
-    END
-DELIMITER;
+FOR EACH ROW 
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
