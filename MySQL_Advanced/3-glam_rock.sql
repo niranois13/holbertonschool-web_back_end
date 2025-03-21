f93ffd2a2b4fe459f4c37ac4c7a579da8f36a981
niranois13 <8784@holbertonstudents.com>
2025-03-21 11:56:01 +0100

    Task3 fix

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- 3. Old school band
 SELECT DISTINCT `band_name`, IFNULL(`split`, 2025) - `formed` AS `lifespan` 
 FROM `metal_bands` 
-WHERE FIND_IN_SET('Glam rock', `style`) 
+WHERE FIND_IN_SET('Glam rock', style) 
 ORDER BY `lifespan` DESC;
