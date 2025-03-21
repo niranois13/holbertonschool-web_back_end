546ed29c1f7439c0e37412f0c6d176d28349b3b1
niranois13 <8784@holbertonstudents.com>
2025-03-21 12:55:35 +0100

    Task3 fix

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- 3. Old school band
 -- lists all bands with Glam rock as main style, ranked by longevity
 SELECT band_name, 
-       (IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
+       (IFNULL(split, 2024) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
