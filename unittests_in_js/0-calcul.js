a2239f4ab7f1f027bd55ea040c312c97e7ab5570
niranois13 <8784@holbertonstudents.com>
2025-03-25 10:17:30 +0100

    Task 0

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,14 @@
-module.exports = function calculateNumber(a, b = 0) {
-  if (isNaN(a) || isNaN(b)){
+module.exports = function calculateNumber(a = 0, b = 0) {
+  const NumA = Number(a)
+  const NumB = Number(b)
+
+  if (a === null || b === null) {
     throw new TypeError()
   }
 
-  return (Math.round(a) + Math.round(b));
+  if (isNaN(NumA) || isNaN(NumB)) {
+    throw new TypeError()
+  }
+
+  return (Math.round(NumA) + Math.round(NumB));
 }
--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -21,6 +21,8 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(2), 2);
     assert.strictEqual(calculateNumber(2.7), 3);
     assert.strictEqual(calculateNumber(-1.2), -1);
+    assert.strictEqual(calculateNumber(0), 0);
+    assert.strictEqual(calculateNumber(-0), 0);
   });
 
   it('should cast non-numbers into numbers', () => {
@@ -29,8 +31,34 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber('1.2', 3.7), 5);
   });
 
+  it('should work with no parameters given and return 0', () => {
+    assert.strictEqual(calculateNumber(), 0);
+  });
+
+  it('should ignore extra args if more than 2 are provided', () => {
+    assert.strictEqual(calculateNumber(2.4, 9.2, 8.4), 11);
+  })
+
   it('should throw typeerror if param is NaN', () => {
     assert.throws(() => calculateNumber('hello'), {
+      name: 'TypeError'
+    });
+    assert.throws(() => calculateNumber(1.2, 'dog'), {
+      name: 'TypeError',
+    });
+    assert.throws(() => calculateNumber('cat', 8.9), {
+      name: 'TypeError'
+    })
+  });
+
+  it('should throw TypeError with null parameters', () => {
+    assert.throws(() => calculateNumber(2.4, null), {
+      name: 'TypeError'
+    });
+    assert.throws(() => calculateNumber(null, 3.7), {
+      name: 'TypeError'
+    });
+    assert.throws(() => calculateNumber(null, null), {
       name: 'TypeError'
     });
   });
