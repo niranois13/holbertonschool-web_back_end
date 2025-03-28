e3c3dabaf1146261c626837a306ef985b6e0c92f
niranois13 <8784@holbertonstudents.com>
2025-03-28 10:56:47 +0100

    Task 8

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -9,3 +9,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('API available on localhost port 7865');
 });
+
+module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,19 +1,25 @@
 const request = require('request');
 const { expect } = require('chai');
+const app = require('./api');
 
 describe('Integration Testing', function () {
-  describe('GET /', () => {
-    it('Code: 200 | Body: Welcome to the payment system', (done) {
-      const options = {
-        url: 'http://localhost:7865',
-        method: 'GET',
-      };
+  let server;
 
-      request(options, function (error, response, body) {
+  before((done) => {
+    server = app.listen(7865, () => done);
+  });
+
+  after(() => {
+    server.close();
+  })
+
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      request('http://localhost:7865', function (error, response, body) {
         expect(response.statusCode).to.equal(200);
         expect(body).to.equal('Welcome to the payment system');
         done();
-      }).catch(done);
+      });
     });
   });
 });
